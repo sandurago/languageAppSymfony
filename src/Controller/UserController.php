6d79c1366d0994b3ca57a907453657d801c0530f
Sandra Liu <sara@frad.in>
2023-10-11 13:50:30 +0200

    Created user.

---
+++ src/Controller/UserController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\User;
+use App\Repository\UserRepository;
+
+class UserController extends AbstractController
+{
+    // have to specify the method. created array of allowed methods.
+    #[Route('/create', name: 'create_user', methods: ['POST'])]
+    public function create(Request $request, EntityManagerInterface $entity_manager): JsonResponse
+    {
+        // get the content of the request. getContent() will give a string;
+        $reqArr = $request->toArray();
+
+        /** @var UserRepository $userRepository */
+        $userRepository = $entity_manager->getRepository(User::class);
+        $user = $userRepository->findOneBy(['nickname' => $reqArr['nickname']]);
+
+        if ($user) {
+            $response = ['message' => 'User already exists.'];
+            $status = 403;
+        } else {
+            $user = new User();
+            $user->setNickname($reqArr['nickname']);
+            $user->setName($reqArr['name']);
+            $user->setPassword($reqArr['password']);
+
+            // Push and save to database
+            $entity_manager->persist($user);
+            $entity_manager->flush();
+
+            $response = ['message' => 'User created.'];
+            $status = 201;
+        }
+
+        return $this->json($response, $status);
+    }
+
+    #[Route('/login', name: 'login_user', methods: ['POST'])]
+    public function login(Request $request, EntityManagerInterface $entity_manager): JsonResponse {
+        $reqArr = $request->toArray();
+
+        // Gets repository of the user to use methods to find in database
+        $userRepository = $entity_manager->getRepository(User::class);
+        // Find user by nickname. If not found, returns NULL
+        $user = $userRepository->findOneBy(['nickname' => $reqArr['nickname']]);
+
+        if (!$user) {
+            $response = ['message' => 'Cannot login. User not found.'];
+            $status = 404;
+        } else {
+            $response = ['message' => 'OK'];
+            $status = 200;
+        }
+        return new JsonResponse($response, $status);
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nickname = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $password = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNickname(): ?string
+    {
+        return $this->nickname;
+    }
+
+    public function setNickname(string $nickname): static
+    {
+        $this->nickname = $nickname;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+}
